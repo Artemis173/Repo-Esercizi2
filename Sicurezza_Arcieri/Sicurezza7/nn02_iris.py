9639f8d4e43e40d79deffa8a124aa895c53eaa57
Artemis173 <edyenwere@gmail.com>
2025-03-05 13:49:46 +0100

    update

--- Sicurezza_Arcieri/Sicurezza7/nn02_iris.py
+++ Sicurezza_Arcieri/Sicurezza7/nn02_iris.py
@@ -14,18 +14,29 @@ res = train_test_split(data, labels,
                        random_state=42)
 train_data, test_data, train_labels, test_labels = res
 
-print()
+def ConvertiY(y):
+    ly = []
+    for vy in y:
+        if vy == 0:
+            ly.append([1,0,0])
+        elif vy == 1:
+            ly.append([0,1,0])
+        else:
+            ly.append([0,0,1])
+    return np.array(ly)
 
-x = np.array([[0, 0], [0, 1], [1, 1]])
-y = np.array([[0],  [1],  [1],   [0]])
-print()
+y = ConvertiY
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(2, activation=tf.keras.activations.relu, input_shape=(2,)))
-model.add(tf.keras.layers.Dense(1, activation=tf.keras.activations.sigmoid))
+model.add(tf.keras.layers.Dense(128, activation=tf.keras.activations.relu, input_shape=(4,)))
+model.add(tf.keras.layers.Dense(32, activation=tf.keras.activations.relu))
+model.add(tf.keras.layers.Dense(1, activation=tf.keras.activations.softmax))
 
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.3), loss=tf.keras.losses.MeanSquaredError(), metrics=[tf.keras.metrics.Accuracy()])
-model.fit(x, y, epochs=100)
+model.fit(X, y, epochs=100)
 
 predictions = model.predict(X)
 print(predictions)
+
+for vx, vy in zip(predictions, y):
+    print(vx, vy)
