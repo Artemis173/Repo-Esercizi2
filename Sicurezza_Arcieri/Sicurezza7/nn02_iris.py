b8268daef95013c0386b2857bf8b92266fe8c963
Artemis173 <edyenwere@gmail.com>
2025-03-05 10:50:04 +0100

    update

--- Sicurezza_Arcieri/Sicurezza7/nn02_iris.py
+++ Sicurezza_Arcieri/Sicurezza7/nn02_iris.py
@@ -2,6 +2,7 @@
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
 import numpy as np
 iris = load_iris()
 
@@ -18,3 +19,13 @@ print()
 x = np.array([[0, 0], [0, 1], [1, 1]])
 y = np.array([[0],  [1],  [1],   [0]])
 print()
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Dense(2, activation=tf.keras.activations.relu, input_shape=(2,)))
+model.add(tf.keras.layers.Dense(1, activation=tf.keras.activations.sigmoid))
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.3), loss=tf.keras.losses.MeanSquaredError(), metrics=[tf.keras.metrics.Accuracy()])
+model.fit(x, y, epochs=100)
+
+predictions = model.predict(X)
+print(predictions)
